f3277071a34e5671cc19dc66ad89dac83359fe29
root <sdimivy014@gmail.com>
2022-06-08 17:25:22 +0900

    from server5 add getdata file

---
+++ datasets/getdata.py
@@ -0,0 +1,25 @@
+from .cpn import CPNSegmentation
+from .cpn_all import CPNALLSegmentation
+from .cpn_six import CPN
+from .median import Median
+from .cpn_aug import CPNaug
+from .cpn_ver import CPNver
+from .cpn_cm import CPNcm
+
+def cpn():
+    ...
+
+def cpn_all():
+    ...
+
+def cpn_six():
+    ...
+
+def cpn_aug():
+    ...
+
+def cpn_cm():
+    ...
+
+def medain():
+    ...
